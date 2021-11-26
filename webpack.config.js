9398158fce1cdd954aedfcc3d1a4a3e8948f9625
Osmar Reyes <osmar@lc.dev>
2021-11-26 16:18:46 -0500

    update build directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	entry: './src/main/index.js',
 	output: {
-		path: path.resolve(__dirname, 'build'),
+		path: path.resolve(__dirname, 'docs'),
 		filename: 'main.js'
 	},
 	devServer: {
